4ff37de495a6d5bbda6c35e920b2a06f1f84ae88
damascus8 <siddharthshrivastav30@gmail.com>
2025-08-22 18:27:04 +0530

    chaning var naming convention

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
